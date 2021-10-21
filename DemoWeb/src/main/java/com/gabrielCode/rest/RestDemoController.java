84db948ef9863169f4f9b5d69dc487874b98716b
dballestin <david.ballestin@gmail.com>
2021-10-21 14:05:52 +0200

    anyadimos RestController basico... devuelve el listado de personas

---
+++ DemoWeb/src/main/java/com/gabrielCode/rest/RestDemoController.java
@@ -0,0 +1,23 @@
+package com.gabrielCode.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrielCode.model.Persona;
+import com.gabrielCode.repo.IPersonaRepo;
+
+@RestController
+public class RestDemoController {
+
+	@Autowired
+	private IPersonaRepo repo;
+	
+	@GetMapping
+	public List<Persona> listar() {
+		return repo.findAll();
+	}
+	
+}
