ca967d474458e8f17feb820eadc0b039b6e539e5
dballestin <david.ballestin@gmail.com>
2021-10-21 15:01:59 +0200

    anyadimos mapping a /personas

--- DemoWeb/src/main/java/com/gabrielCode/rest/RestDemoController.java
+++ DemoWeb/src/main/java/com/gabrielCode/rest/RestDemoController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielCode.model.Persona;
 import com.gabrielCode.repo.IPersonaRepo;
 
 @RestController
+@RequestMapping("/personas")
 public class RestDemoController {
 
 	@Autowired
