258dd05864d1ce941faa9839d305a8973ed0c08b
dballestin <david.ballestin@gmail.com>
2021-10-21 13:34:00 +0200

    anyadimos conectivad a BD con JPA

---
+++ DemoWeb/src/main/java/com/gabrielCode/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.gabrielCode.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gabrielCode.model.Persona;
+import com.gabrielCode.repo.IPersonaRepo;
+
+@Controller
+public class DemoController {
+
+	@Autowired
+	private IPersonaRepo repo;
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		Persona per = new Persona(1, "Gabriel Casas");
+		repo.save(per);
+		
+		model.addAttribute("name", name);
+		
+		return "greeting";
+	}
+
+}
---
+++ DemoWeb/src/main/java/com/gabrielCode/repo/IPersonaRepo.java
@@ -0,0 +1,9 @@
+package com.gabrielCode.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielCode.model.Persona;
+
+public interface IPersonaRepo extends JpaRepository<Persona, Integer> {
+
+}
