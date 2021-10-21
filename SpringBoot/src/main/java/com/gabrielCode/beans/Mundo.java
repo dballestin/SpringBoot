ba890fd45f400976636bc0e524d6d7215904ca7e
dballestin <david.ballestin@gmail.com>
2021-10-21 11:14:13 +0200

    proyecto spring con arquetipo maven

---
+++ SpringBoot/src/main/java/com/gabrielCode/beans/Mundo.java
@@ -0,0 +1,13 @@
+package com.gabrielCode.beans;
+
+public class Mundo {
+	private String saludo;
+
+	public String getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+}
